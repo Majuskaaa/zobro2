a04424e0d3042388200f939cff07c57806f23ca8
Majuskaaa <ma.i.a@seznam.cz>
2019-09-20 08:48:55 +0200

    aktualizace: pouze Kamčatka

--- components/animals/medvedKamcatsky.adult.js
+++ components/animals/medvedKamcatsky.adult.js
@@ -23,7 +23,7 @@ var AnimalDetail = React.createClass({
     return (
       <AnimalTemplate firstIndex={[2]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator}>
           <AnimalText>
-            Medvěd kamčatský (lat. <Text style={styles.ctextItalic}>Ursus arctos beringianus</Text>) je největší euroasijská suchozemská šelma, a jak už sám název napovídá, pochází z&nbsp;Kamčatky, poloostrova na Dálném východě v&nbsp;Rusku. Vyskytuje se také na přilehlých ostrovech a poloostrovech. V&nbsp;České republice ho můžeme najít jen v&nbsp;naší zoo. Příroda, ze které k&nbsp;nám přicestoval, je skutečně krásná – většinu jejího povrchu tvoří sopky, gejzíry a rozsáhlé lesy. 
+            Medvěd kamčatský (lat. <Text style={styles.ctextItalic}>Ursus arctos beringianus</Text>) je největší euroasijská suchozemská šelma, a jak už sám název napovídá, pochází z&nbsp;Kamčatky, poloostrova na Dálném východě v&nbsp;Rusku. Vyskytuje se také na přilehlých ostrovech a poloostrovech. V&nbsp;České republice ho můžeme najít kromě naší zoo ještě v&nbsp;děčínské. Příroda, ze které k&nbsp;nám přicestoval, je skutečně krásná – většinu jejího povrchu tvoří sopky, gejzíry a rozsáhlé lesy. 
           </AnimalText>
           <AnimalText>
             Medvěd kamčatský, extrémně silný a odolný poddruh medvěda hnědého, dorůstá výšky až 130&nbsp;cm v&nbsp;kohoutku a délky až 280&nbsp;cm. Vážit může až 750&nbsp;kg. Má rychlé reakce a je mrštný, jeho zuby dokážou překousnout jakoukoli kost, svými tlapami zvládá valit balvany, vyšplhá téměř na jakkoli kolmý svah i&nbsp;do koruny silného stromu a vydrží dlouhé hodiny sedět v&nbsp;ledové vodě. Je také vynikajícím plavcem a potápěčem. Na krátké vzdálenosti dosahuje v&nbsp;běhu rychlosti až 45–60&nbsp;km/h a za den dokáže ujít 100&nbsp;km. Fascinující je i&nbsp;jeho orientační smysl – za potravou putuje stovky kilometrů a pak se bez jediného zaváhání vrátí zpět do své domovské oblasti. Je tedy pochopitelné, že na souši nemá kromě člověka přirozeného nepřítele. Ve volné přírodě se medvěd kamčatský dožívá 20 až 30 let, v&nbsp;lidské péči až 50 let.
@@ -37,7 +37,7 @@ var AnimalDetail = React.createClass({
           </AnimalText>
         <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />
           <AnimalText>
-            Zoo Brno chová v&nbsp;současnosti samce Jelizara a samici Kamčatku, kteří spolu zplodili již tři mláďata. Samečci Kuba a Toby se narodili 30.&nbsp;ledna 2012 a v&nbsp;prosinci roku 2014 odcestovali do soukromé ukrajinské zoo. V&nbsp;současné době se svými rodiči sdílí výběh i&nbsp;sameček Bruno, který se narodil 29.&nbsp;ledna 2016.
+            Zoo Brno chová v&nbsp;současnosti pouze samici Kamčatku, která přivedla se samcem Jelizarem na svět tři mláďata. Samečci Kuba a Toby se narodili 30.&nbsp;ledna&nbsp;2012 a v&nbsp;prosinci roku&nbsp;2014 odcestovali do soukromé ukrajinské zoo. Třetí kluk, sameček Bruno, se narodil 29.&nbsp;ledna&nbsp;2016 a momentálně se nachází v&nbsp;již zmíněné zoo v&nbsp;Děčíně.
           </AnimalText>
       </AnimalTemplate>
     );
