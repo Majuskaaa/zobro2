58612f72d34e3fed225c31c5bb55c183307fb074
Majuskaaa <ma.i.a@seznam.cz>
2019-09-20 10:28:03 +0200

    aktualizováno: pouze Cora

--- components/animals/medvedLedni.child.js
+++ components/animals/medvedLedni.child.js
@@ -26,31 +26,31 @@ var AnimalDetail = React.createClass({
           Milé děti,
         </AnimalText>
         <AnimalText>
+        </AnimalText>
+        <AnimalText>
+          jmenuji se Noria a jsem zatím posledním ledním medvídětem narozeným v&nbsp;této zoo. Mými rodiči jsou Cora a Umca, dost možná už jste o&nbsp;nich slyšeli. Narodila jsem se v&nbsp;listopadu&nbsp;2015. Oběma mým rodičům bylo v&nbsp;té době sedmnáct let.
         </AnimalText>      
         <AnimalText>
-          jmenuji se Noria a jsem zatím nejmladším medvídětem místních ledních medvědů, jimiž jsou Cora a Umca. Narodila jsem se v&nbsp;listopadu 2015. Oběma mým rodičům bylo v&nbsp;té době sedmnáct let.
+          Říkáte si: „Tak mladí?!“ U&nbsp;nás medvědů je to trošku jiné než u&nbsp;lidí, dospíváme už ve třetím až pátém roce života a i&nbsp;děti míváme mnohem dříve než vy. Tak třeba když se narodili mí nejstarší bráchové, Bill a Tom, bylo našim teprve devět let! Kometu a Nanuka měli ve čtrnácti – a pak už jsem se narodila já. A ještě vám řeknu jednu zajímavost, všichni jsme se narodili ve stejném měsíci – v&nbsp;listopadu.
         </AnimalText>
         <AnimalText>
-          Říkáte si: „Tak mladí?!“ U&nbsp;nás zvířat je to trošku jiné než u&nbsp;lidí, dospíváme už ve třetím až pátém roce života a i&nbsp;děti míváme mnohem dříve než vy. Tak třeba když se narodili mí nejstarší bráchové, Bill a Tom, bylo našim teprve devět let! Kometu a Nanuka měli ve čtrnácti – a pak už jsem se narodila já. A ještě vám řeknu jednu zajímavost, všichni máme narozeniny ve stejném měsíci – v&nbsp;listopadu.
-        </AnimalText>
-        <AnimalText>
-          O&nbsp;mých starších sourozencích mi vyprávěla maminka před spaním, když jsem byla malá. Stejně tak o&nbsp;tátovi. Občas ho zaslechnu z&nbsp;vedlejšího výběhu, ale k&nbsp;němu mě chovatelé nepustí. Asi se bojí, že bych ho moc zlobila. Což je pravda. Zlobit, to já umím moc dobře! Ale maminka už má s&nbsp;výchovou mláďat bohaté zkušenosti, takže nezvlčím. Maximálně zmedvědím.
+          O&nbsp;mých starších sourozencích mi vyprávěla maminka před spaním, když jsem byla malá. Stejně tak o&nbsp;tátovi. Občas jsem ho zaslechla z&nbsp;vedlejšího výběhu, ale k&nbsp;němu mě chovatelé nikdy nepustili. Asi se báli, že bych ho moc zlobila. Což je pravda. Zlobit, to jsem jako malá uměla moc dobře! Ale maminka už měla s&nbsp;výchovou mláďat bohaté zkušenosti, takže jsem nezvlčela. Jen trochu zmedvěděla.
         </AnimalText>
       <InPageImage indexes={[1]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} />      
         <AnimalText>
-          Když jsem se narodila, vážila jsem jen o&nbsp;něco víc než půl pytlíku mouky. Vypadala jsem spíš jak morče než jak medvěd. Maminka mě tulila k&nbsp;sobě, abych byla v&nbsp;teple a mohla se najíst, kdykoliv jsem měla hlad. A já jsem při tom blahem hrčela jak traktor. Když totiž medvíďata sají mléko, opravdu to zní jako startující traktor! Ale po pár měsících už jsem začala běhat po venku a rostla jak z&nbsp;vody. I&nbsp;když je možné, že jsem opravdu rostla z vody – strašně ráda plavu, a ještě raději házím do bazénu balon a pak za ním skáču. Jsem totiž věrná svému jménu – jsem medvěd lední, a voda je přeci taky led, jen rozpuštěný.
+          Když jsem se narodila, vážila jsem jen o&nbsp;něco víc než půl pytlíku mouky. Vypadala jsem spíš jak morče než jak medvěd. Maminka mě tulila k&nbsp;sobě, abych byla v&nbsp;teple a mohla se najíst, kdykoliv jsem měla hlad. A já jsem při tom blahem hrčela jak traktor. Když totiž medvíďata sají mléko, opravdu to zní jako startující traktor! Ale po pár měsících už jsem začala běhat po venku a rostla jak z&nbsp;vody. I&nbsp;když je možné, že jsem opravdu rostla z&nbsp;vody – odmala strašně ráda plavu, a ještě raději házím do bazénu balon a pak za ním skáču. Jsem totiž věrná svému jménu – jsem medvěd lední, a voda je přeci taky led, jen rozpuštěný.
         </AnimalText>
       <InPageImage indexes={[0]} thumbnails={THUMBNAILS} images={IMAGES} navigator={this.props.navigator} /> 
         <AnimalText>
-          Na závěr by se hodilo dodat ještě nějaké moudro. Tak třeba: zlobit můžete, ale jen trošku. Jinak buďte na rodiče hodní, vždyť oni si to zaslouží.
-        </AnimalText>
-        <AnimalText>
-          Mějte se hezky a zas někdy příště!
+          Když jsem vyrostla, bylo načase, abych se podívala do světa. V&nbsp;srpnu&nbsp;2018 jsem tedy odjela do zoo v&nbsp;německém Rostocku, kde momentálně bydlím se svým kamarádem Sizzelem. V&nbsp;Brně však můžete potkat moji maminku Coru. A&nbsp;nemusíte mít ani velké štěstí, abyste se přesvědčili, po kom mám tu svoji zálibu v&nbsp;cachtání se ve vodě :–).
         </AnimalText>
         <AnimalText>
         </AnimalText>
         <AnimalText>
-          Vaše Noria
+          S&nbsp;láskou
+        </AnimalText>
+        <AnimalText>
+          vaše Noria
         </AnimalText>
       </AnimalTemplate>
     );
